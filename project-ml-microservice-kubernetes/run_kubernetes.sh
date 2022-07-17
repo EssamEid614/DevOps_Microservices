4335caa1c0d590bf7e2866caef32ac13cd5ea377
Essameldeen Youssef <eeid@newstore.com>
2022-07-18 01:59:49 +0200

    update

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -4,15 +4,17 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=essameid614/helloworld
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl create deployment helloworld --image=$dockerpath --port=8000
 
 # Step 3:
 # List kubernetes pods
+kubectl get nodes
 
 # Step 4:
 # Forward the container port to a host
 
+kubectl port-forward pod/helloworld-df6dc6cdf-ghwvc --address 0.0.0.0 8000:8000
--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -5,11 +5,13 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=essameid614/helloworld
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker tag helloworld $dockerpath
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath:latest
